a0065e51bab657947eeae8fb6c508d1415b74f6d
luciferDisciple <waizer12@gmail.com>
2021-09-05 23:39:53 +0200

    installation script
    fix: TexGyreCursor Regular font, that the script previously depended on,
    added to repository

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+data_dir="$HOME/.local/share/txt2pdf"
+enscript_profile="$HOME/.enscriptrc"
+
+dependencies=(
+	iconv
+	enscript
+	ps2pdf
+)
+
+for dep in "${dependencies[@]}"; do
+	type $dep &>/dev/null || {
+		echo "Dependency not satisfied: $dep. Aborting.";
+		exit 1
+	}
+done
+
+IFS=: paths=($PATH)
+
+echo "Where do you want to place the script?"
+select chosen_dir in "${paths[@]}" quit; do
+	[[ "$chosen_dir" == quit ]] && exit 1
+	[[ -z "$chosen_dir" ]] && continue
+	read -p "Do you want to put txt2pdf script '$chosen_dir'? [Y/n] " \
+		confirm_choice
+	[[ "$confirm_choice" != Y ]] &&
+		[[ "$confirm_choice" != y ]] &&
+		continue
+	if [[ -e "$chosen_dir"/txt2pdf ]]; then
+		echo "File '$chosen_dir/txt2pdf' exists."
+		read -p "Do you want to overwrite it? [Y/n] " overwrite
+		[[ "$overwrite" != Y ]] && [[ "$overwrite" != y ]] && continue
+		rm "$chosen_dir"/txt2pdf || continue
+	fi
+	cp 'txt2pdf.sh' "$chosen_dir"/txt2pdf
+	break
+done
+
+if [[ ! -e "$data_dir" ]]; then
+	mkdir "$data_dir"
+fi
+
+echo "Installing fonts to '$data_dir'"
+cp -r resources/fonts "$data_dir"
+
+echo "Creating font map for enscript"
+(cd "$data_dir/fonts"; mkafmmap *.afm &>/dev/null ||
+	echo 'An error occured while creating enscript font map. Aborting.'
+) || exit 1
+
+if [[ -e "$HOME/.enscriptrc" ]]; then
+	echo "Found existing enscript configuration file."
+	enscriptrc_bak="$enscript_profile".`date +%Y%m%d%H%M%S`.bak
+	echo "Moving '$enscript_profile' to '$enscriptrc_bak'"
+	mv "$enscript_profile" "$enscriptrc_bak"
+fi
+
+echo "Creating '$enscript_profile'"
+
+echo "Clean7Bit: 0" >>"$enscript_profile"
+echo "AFMPath: $data_dir/fonts:/usr/local/share/enscript" >>"$enscript_profile"
+
+echo 'Installation complete'
